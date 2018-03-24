0457f9f9a1448f288ca3469fd64f5009a8c873a1
Chtwin <37251146+Chtwin@users.noreply.github.com>
2018-03-24 22:09:21 +0100

    MàJ IA + define
    Modification en rapport avec le précédent commit.

--- arène_code/constantes.h
+++ arène_code/constantes.h
@@ -16,7 +16,9 @@
 #define BLUE_BONUS  5
 #define RED_BONUS   20
 #define ECART 33
-#define NB_PLAYER 3         // vous pouvez mettre le nombre de joueurs que vous voulez
+#define NB_PLAYER 20         // vous pouvez mettre le nombre de joueurs que vous voulez
+#define DEGAT_EPEE 5
+#define RECUP_RUBIS 3
 
 /*
     Ajout de la structure propre à chaque IA
@@ -35,7 +37,7 @@ struct Player
 };
 
 enum {UP, DOWN, LEFT, RIGHT,ANIM_UP1,ANIM_UP2,ANIM_UP3,ANIM_DOWN1,ANIM_DOWN2,ANIM_DOWN3,ANIM_LEFT1,ANIM_LEFT2,ANIM_LEFT3,ANIM_RIGHT1,ANIM_RIGHT2,ANIM_RIGHT3, HIT_UP, HIT_DOWN, HIT_LEFT, HIT_RIGHT, SHIELD};
-enum {HAUT, BAS, GAUCHE, DROITE, EPEE_HAUT,EPEE_BAS,EPEE_DROITE,EPEE_GAUCHE, PARER,BOMBE};
+enum {HAUT, BAS, GAUCHE, DROITE, EPEE_HAUT,EPEE_BAS,EPEE_DROITE,EPEE_GAUCHE, PARER,BOMBE};  /// Différentes actions que peuvent faire les IA's
 enum{GREEN_RUPEE,BLUE_RUPEE,RED_RUPEE,VIDE,MUR,IA,GANON};
 int play (SDL_Surface* screen);
 int movePlayer (int maps[][NB_BLOCS_HAUTEUR], SDL_Rect *position, int direction, Mix_Chunk *s_ruppes);
@@ -47,7 +49,7 @@ void ganon_move(int maps[][NB_BLOCS_HAUTEUR], Mix_Chunk *s_degat, Player links[N
 void setup_pictures (SDL_Surface *link[21],SDL_Surface *rupees[3],SDL_Surface *ganon[5], SDL_Surface *zelda[6], SDL_Surface *guard[16], SDL_Surface *skull[4],SDL_Surface *daruina[4],SDL_Surface *granma[4],SDL_Surface *koume[4],SDL_Surface *maple [4],SDL_Surface *oldman[4],SDL_Surface *nayru[4],SDL_Surface *saria[4],SDL_Surface *sheik[4], SDL_Surface *ruto[4], SDL_Surface *rauru[4]);
 void setup_map (int maps[][NB_BLOCS_HAUTEUR]);
 void timer (char temps[],char score[],int time,int lastTime,int stime,int mtime,int points);
-int win (bool survivant, SDL_Surface* screen, Mix_Music *gerudo, Player links[NB_PLAYER], int continuer);
+int win (bool survivant, SDL_Surface* screen, Mix_Music *gerudo, Player links[NB_PLAYER], int continuer, int tours);
 void animation(SDL_Surface* screen, SDL_Surface *zelda[6], SDL_Surface *skull[4],SDL_Surface *daruina[4],SDL_Surface *granma[4],SDL_Surface *koume[4],SDL_Surface *maple [4],SDL_Surface *oldman[4],SDL_Surface *nayru[4],SDL_Surface *saria[4],SDL_Surface *sheik[4], SDL_Surface *ruto[4], SDL_Surface *rauru[4], int tours);
 void garde(SDL_Surface* screen, SDL_Surface *guard[16], int tours);
 int test_ia(int maps[][NB_BLOCS_HAUTEUR]);
@@ -59,12 +61,12 @@ bool test_class(int maps[][NB_BLOCS_HAUTEUR], Player links[NB_PLAYER]);
 /*
 Fonction qui renvoie un entier pour faire bouger l'IA
 */
-int ia_1(int maps_ia[][NB_BLOCS_HAUTEUR], int x, int y, int points, int item, int bouclier, int orientation, int tours)
+int ia_1(int maps_ia[][NB_BLOCS_HAUTEUR], int x, int y, int points, int item, int tours)
 {
     return rand()%10;
 }
 
-int ia_2(int maps_ia[][NB_BLOCS_HAUTEUR], int x, int y, int points, int item, int bouclier, int orientation, int tours)
+int ia_2(int maps_ia[][NB_BLOCS_HAUTEUR], int x, int y, int points, int item, int tours)
 {
     return rand()%10;
 }
