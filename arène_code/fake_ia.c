f1c1f7d7fb4669afa8ae2145f8a27c04872f902f
Louis Cherel <cherel.louis@gmail.com>
2018-04-14 10:32:05 +0200

    Ajout des définitions des IA et du calcul des IA présentes

---
+++ arène_code/fake_ia.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>  
+#include <math.h>
+#include "constantes.h"
+
+int ia_1 (int maps_ia[][NB_BLOCS_HAUTEUR], int x, int y, int points, int item, int tours) {
+  return HAUT;
+}
